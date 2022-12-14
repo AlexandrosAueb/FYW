7d5f29cf5025cab18a81a068ac2c9f79e4f73e20
ALEXANDROS PAPPAS <80106020+AlexandrosAueb@users.noreply.github.com>
2022-12-14 19:42:58 +0200

    Added: Order & OrderDAO Classes

--- fyw/OrdersDAO.java
+++ fyw/OrdersDAO.java
@@ -38,7 +38,7 @@ public class OrdersDAO {
                         rs.getInt("Estimated_time"),
                         rs.getInt("Delivered_time"),
                         rs.getString("Status"),
-                        rs.getInt("Order_Source"),
+                        rs.getInt("Order Source"),
                         rs.getInt("employees_ID")
                         ));
 			}
@@ -62,10 +62,123 @@ public class OrdersDAO {
 
 		return orders;
 
-	} // End of getEmployees
+	} // End of getOrders
+
+
+	public Orders findOrders(int id) throws Exception {
+		Connection con = null;
+		DB db = new DB();
+		String sql = "SELECT * FROM orders";
+		Orders order = null;
+
+		try {
+			// open connection and get Connection object
+			con = db.getConnection();
+
+			PreparedStatement stmt = con.prepareStatement(sql);
+
+			// execute the SQL statement (QUERY - SELECT) and get the results in a
+			// ResultSet)
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				if (id == rs.getInt("Orders_ID")) {
+					order = new Orders(rs.getInt("Orders_ID"),
+					rs.getString("Date"),
+					rs.getString("Customer_Name"),
+					rs.getString("Address"),
+					rs.getString("Products"),
+					rs.getDouble("Price"),
+					rs.getInt("Estimated_time"),
+					rs.getInt("Delivered_time"),
+					rs.getString("Status"),
+					rs.getInt("Order Source"),
+					rs.getInt("employees_ID")
+					);
+					break;
+				}
+					
+			}
+
+			rs.close(); // closing ResultSet
+			stmt.close(); // closing PreparedStatement
+			db.close(); // closing connection
+
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+
+		} finally {
+
+			try {
+				db.close();
+			} catch (Exception e) {
+
+			}
+
+		}
+
+		return order;
+
+	} // End of findOrder
 
 
 
+	public List<Orders> findOrdersByStatus(String status) throws Exception {
+		
+		Connection con = null;
+		DB db = new DB();
+		String sql = "SELECT * FROM orders WHERE status = ?";
+		List<Orders> orders = new ArrayList<Orders>();
 
-    
-}
+
+		try {
+			// open connection and get Connection object
+			con = db.getConnection();
+
+			PreparedStatement prepareStatement = db.getConnection().prepareStatement(sql);
+            prepareStatement.setString(1, status);
+
+			// execute the SQL statement (QUERY - SELECT) and get the results in a
+			// ResultSet)
+			ResultSet rs = prepareStatement.executeQuery();
+
+			while (rs.next()) {
+				orders.add(new Orders(rs.getInt("Orders_ID"),
+						rs.getString("Date"),
+                        rs.getString("Customer_Name"),
+                        rs.getString("Address"),
+                        rs.getString("Products"),
+                        rs.getDouble("Price"),
+                        rs.getInt("Estimated_time"),
+                        rs.getInt("Delivered_time"),
+                        rs.getString("Status"),
+                        rs.getInt("Order Source"),
+                        rs.getInt("employees_ID")
+                        ));
+			}
+					
+
+			rs.close(); // closing ResultSet
+			prepareStatement.close(); // closing PreparedStatement
+			db.close(); // closing connection
+
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+
+		} finally {
+
+			try {
+				db.close();
+			} catch (Exception e) {
+
+			}
+
+		}
+
+		return orders;
+
+	} // End of findOrderByStatus
+
+} 
+
+	
